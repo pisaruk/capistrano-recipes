40a3f60f713736177d4d761073b31f97c902079d
pisaruk <pisaruk@gmail.com>
2013-06-07 16:02:02 -0300

    remove some hooks

--- lib/recipes/hooks.rb
+++ lib/recipes/hooks.rb
@@ -1,13 +1,15 @@
 # Common hooks for all scenarios.
 Capistrano::Configuration.instance.load do
   after 'deploy:setup' do
+    db.create_yml
     app.setup
     bundler.setup if Capistrano::CLI.ui.agree("Do you need to install the bundler gem? [Yn]")
   end
-    
+
   after "deploy:update_code" do
-    symlinks.make
-    bundler.install
+    db.symlink
+    #  symlinks.make
+    #  bundler.install
   end
 end
 
